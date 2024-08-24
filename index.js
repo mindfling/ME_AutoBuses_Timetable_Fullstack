24cf4ebf35c39325d11366c1bd68ca46b3790354
Nepochtov Dmitry <dimendroider@gmail.com>
2024-08-24 17:40:13 +0300

    feat work with luxon

--- index.js
+++ index.js
@@ -4,66 +4,128 @@ import path from "node:path";
 
 import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";
-const __filename = fileURLToPath(import.meta.url);
-console.log('__filename: ', __filename);
-const __dirname = dirname(__filename);
-console.log('__dirname: ', __dirname);
-
+import { DateTime } from "luxon";
+const __filename = fileURLToPath(import.meta.url); // ??? в модулях нужно отдельно указывать
+const __dirname = dirname(__filename); // ??? в модулях нужно отдельно указывать
 
 const port = 3000;
 const LOCAL = "http://localhost";
+const URL = `${LOCAL}:${port}`;
+
+// * загружаем всё расписание
+const loadBuses = async () => {
+  const data = await readFile(path.join(__dirname, "buses.json"), {
+    encoding: "utf-8",
+  });
+  return JSON.parse(data);
+};
+
+const getNextDeparture = (bus) => {
+  const { firstDepartureTime, frequencyMinutes } = bus; //todo
+
+  const now = DateTime.now().setZone("UTC");
+
+  const [hours, minutes] = firstDepartureTime.split(":").map(Number); // перебираем parseInt(n)
+
+  let departure = DateTime.now().set({
+    hours,
+    minutes,
+    seconds: 0,
+    milliseconds: 0,
+  })
+  .setZone('UTC');
+
+  // console.log("\ndeparture: ", departure);
+
+  if (now > departure) {
+    console.log("now is > departure");
+    departure = departure.plus({ minutes: frequencyMinutes });
+  } else {
+    console.warn("now is < departure");
+    console.log(bus);
+  }
+
+  const endOfDay = DateTime.now()
+    .set({ hours: 23, minutes: 59, seconds: 59 })
+    .setZone("UTC");
+
+  if (departure > endOfDay) {
+    console.log("departure is > endOfDay");
+    departure = departure
+      .startOf("day")
+      .plus({ days: 1 })
+      .set({ hours, minutes });
+  } else {
+    console.warn("departure is today < endOfDay");
+  }
+  console.log('departure: ', departure);
+
+  return departure;
+};
+
+// вычисляем время отправляения автобуса
+const sendUpdatedData = async () => {
+  const buses = await loadBuses();
+
+  const now = DateTime.now().setZone("UTC");
+  console.log("now: UTC", now);
+
+  const updatedBuses = buses.map((bus) => {
+    //todo
+    getNextDeparture(bus);
+
+    console.log(
+      bus.id,
+      "автобус",
+      bus.busNumber,
+      bus.firstDepartureTime,
+      bus.frequencyMinutes
+    );
+  });
+};
+
+const updateBuses = sendUpdatedData(); // !!!!!
 
 const app = express();
 
-const body = `<body style="background:#111; color:#0f3; font-family:sans-serif;">
-  <h1>Hello World!!!</h1>
-  <p>THis is our hello world answer</p>
-</body>`;
-
-const answer = ({title}, {text}) => {
-  console.log({ title }, { text });
-
-  return `<body style="background:#111;color:#0f3;font-family:sans-serif;font-size:10px;">
-  <h1>${title}</h1>
-  ${text ? `<p>${text}</p>` : ""}
-</body>`;
-};
-
-app.get("/next-departure", (req, res) => {
-  res.send(
-    answer({
-      title: "timetable next departure",
-    })
-  );
+// ** CORS Headers
+app.use((req, res, next) => {
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", `${LOCAL}:5500`);
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:PORT');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  // Помните, использование звёздочек в качестве маски может быть рискованным.
+  next();
 });
 
-const name = 'aoue'
+// * http://localhost:3000/timetable
+app.get("/timetable", async (req, res) => {
+  console.log("all time table");
+  const data = await loadBuses();
+  res.send(data); // ???
+  // res.send(JSON.stringify(data)); // ???
+});
 
-// http://localhost:3000/hello -> hello world
+// next-departure
+app.get("/next", async (req, res) => {
+  console.log("next");
+  const data = await loadBuses();
+  res.send(data); // ???
+  // res.send(JSON.stringify(data)); // ???
+});
+
+// * http://localhost:3000/hello -> hello world
 app.get("/hello", (req, res) => {
   console.log("helllo");
-  // res.send("<h1>Hello World!!!</h1>");
-  res.send(
-    answer({
-      text: "Это тектс приветствия",
-      title: "Заголовок текста",
-    })
-  );
+  res.send("Заголовок приветствия");
 });
-
-// * расписание здесь
-const loadBuses = async () => {
-  const data = await readFile("buses.json", {
-    encoding: "utf-8",
-  });
-  const json = JSON.parse(data);
-  console.log("json: ", json);
-  return json;
-};
 
 // launch server
 app.listen(port, () => {
-  console.log(`THis server is running on ${LOCAL}:${port}\n`);
+  console.log(`\nTHis server is running on ${LOCAL}:${port}\n`);
 });
 
 console.log("Hello server");
